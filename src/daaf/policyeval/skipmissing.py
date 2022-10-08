94db7f597370d179fe48ef5196d6d4117066ea58
guilherme <guilherme@spotify.com>
2022-10-08 22:18:49 +0200

    First visit MC with skip missing

--- src/daaf/policyeval/skipmissing.py
+++ src/daaf/policyeval/skipmissing.py
@@ -3,6 +3,7 @@ Policy evaluation functions modified for Cumulative Periodic Rewards.
 """
 
 
+import collections
 import copy
 from typing import Any, Callable, Generator, Tuple
 
@@ -88,5 +89,96 @@ def cpr_sarsa_prediction(
             )
 
         # need to copy qtable because it's a mutable numpy array
+        yield len(experiences), copy.deepcopy(qtable)
+
+
+def cpr_first_visit_monte_carlo_action_values(
+    policy: policies.PyQGreedyPolicy,
+    environment: py_environment.PyEnvironment,
+    num_episodes: int,
+    gamma: float,
+    state_id_fn: Callable[[Any], int],
+    action_id_fn: Callable[[Any], int],
+    initial_qtable: np.ndarray,
+    reward_period: int,
+    generate_episodes: Callable[
+        [
+            py_environment.PyEnvironment,
+            py_policy.PyPolicy,
+            int,
+        ],
+        Generator[trajectory.Trajectory, None, None],
+    ] = envplay.generate_episodes,
+) -> Generator[Tuple[int, Array, float], None, None]:
+    """
+    First-Visit Monte Carlo Prediction.
+    Estimates Q(s, a) for a fixed policy pi.
+    Source: http://www.incompleteideas.net/book/ebook/node52.html
+
+    Args:
+        policy: A target policy, pi, whose value function we wish to evaluate.
+        environment: The environment used to generate episodes for evaluation.
+        num_episodes: The number of episodes to generate for evaluation.
+        alpha: The learning rate.
+        gamma: The discount rate.
+        state_id_fn: A function that maps observations to an int ID for
+            the Q(S, A) table.
+        action_id_fn: A function that maps actions to an int ID for
+            the Q(S, A) table.
+        initial_qtable: A prior belief of Q(S, A) estimates.
+        event_mapper: A function that generates trajectories from a given trajectory.
+            This is useful in cases where the caller whishes to apply
+            a transformation to experience logs.
+            Defautls to `envplay.identity_replay`.
+
+    Yields:
+        A tuple of steps (count) and q-table.
+
+    Note: the first reward (in the book) is R_{1} for R_{0 + 1};
+    So index wise, we subtract them all by one.
+    """
+
+    def visit_key(experience) -> Tuple[int, int]:
+        return state_id_fn(experience.observation), action_id_fn(experience.action)
+
+    # first state and reward come from env reset
+    qtable = copy.deepcopy(initial_qtable)
+    state_action_updates = collections.defaultdict(int)
+    state_action_visits_remaining = collections.defaultdict(int)
+
+    for _ in range(num_episodes):
+        # This can be memory intensive, for long episodes and large state/action representations.
+        _experiences = list(generate_episodes(environment, policy, num_episodes=1))
+        # reverse list and ammortize state visits
+        experiences = []
+        while len(_experiences) > 0:
+            experience = _experiences.pop()
+            state_action_visits_remaining[visit_key(experience)] += 1
+            experiences.append(experience)
+
+        episode_return = 0
+        for reverse_step, experience in enumerate(experiences):
+            key = visit_key(experience)
+            state_action_visits_remaining[key] -= 1
+            step = len(experiences) - reverse_step
+            if (step - 1) % reward_period != 0:
+                continue
+
+            state_id, action_id = key
+            reward = experience.reward
+            episode_return = gamma * episode_return + reward
+
+            if state_action_visits_remaining[key] == 0:
+                state_action_updates[key] += 1
+                if state_action_updates[key] == 1:
+                    # first value
+                    qtable[state_id, action_id] = episode_return
+                else:
+                    qtable[state_id, action_id] = qtable[state_id, action_id] + (
+                        (episode_return - qtable[state_id, action_id])
+                        / state_action_updates[key]
+                    )
+
+        # need to copy values because it's a mutable numpy array
         yield len(experiences), copy.deepcopy(qtable)
 
