adee806aad0289b64407f6a7f0379f7ffa096378
guilherme <guilherme@spotify.com>
2023-12-16 16:21:06 +0000

    wip: state values aggregation

--- src/daaf/policyeval/results_agg_pipeline.py
+++ src/daaf/policyeval/results_agg_pipeline.py
@@ -1,16 +1,11 @@
 """
-Data aggregation pipeline to compute
-metrics from experiments.
-
-Metrics
-Accuracy of V(s) @ K (episodes)
-Monotonic correlation of V(s) @ K
-Accuracy of R(s,a)
+Data aggregation pipeline to compute.
+Aggregates results from multiple
+runs from each experiment.
 """
 
 
 import copy
-import dataclasses
 import json
 import logging
 import os.path
@@ -25,15 +20,6 @@ import tensorflow as tf
 from ray.data import aggregate
 
 import daaf
-
-
-@dataclasses.dataclass(frozen=True)
-class Stat:
-    """ """
-
-    mean: float
-    stddev: float
-    p95_ci: float
 
 
 class StateValueAggretator(aggregate.AggregateFn):
@@ -57,28 +43,36 @@ class StateValueAggretator(aggregate.AggregateFn):
         del key
         return {}
 
-    def _merge(self, acc_left: AggType, acc_right: AggType) -> AggType:
-        acc = copy.deepcopy(acc_left)
-        for key, value in acc_right.items():
-            if key in acc:
-                acc[key]["state_values"].extend(value["state_values"])
-            else:
-                acc[key] = copy.deepcopy(acc_right[key])
-        return acc
-
     def _accumulate_row(self, acc: AggType, row: Row) -> AggType:
-        key = (row["experiment_id"], row["episode"])
+        """
+        Add a single row to aggregation.
+        """
         new_acc = copy.deepcopy(acc)
-        if key not in acc:
-            new_acc[key] = {
+        if row["experiment_id"] not in acc:
+            new_acc[row["experiment_id"]] = {
                 "meta": row["meta"],
-                "experiment_id": row["experiment_id"],
                 "state_values": [],
             }
-        new_acc[key]["state_values"].append(row["info"]["state_values"])
+        new_acc[row["experiment_id"]]["state_values"].append(
+            row["info"]["state_values"]
+        )
         return new_acc
 
+    def _merge(self, acc_left: AggType, acc_right: AggType) -> AggType:
+        """
+        Combine two accumulators.
+        """
+        acc = copy.deepcopy(acc_left)
+        for key, value in acc_right.items():
+            if key not in acc:
+                acc[key] = copy.deepcopy(acc_right[key])
+            acc[key]["state_values"].extend(value["state_values"])
+        return acc
+
     def _finalize(self, acc: AggType) -> Any:
+        """
+        Project final output.
+        """
         return acc
 
 
@@ -95,18 +89,6 @@ def main():
     with ray.init(runtime_env=ray_env) as context:
         logging.info("Ray Context: %s", context)
         logging.info("Ray Nodes: %s", ray.nodes())
-        # create io placement group
-        # to limit resources on data reading
-        # pg_io = placement_group([{"CPU": 2}])
-        # ray.get(pg_io.ready())
-        # output = pipeline.remote(
-        #     ray.data.from_items(paths).flat_map(
-        #         fn=lambda row: ray.get(
-        #             parse_experiment_logs.remote(row["item"])
-        #         ).iter_rows(),
-        #     )
-        # )
-        # ray.get(output).write_json(f"/tmp/output/{int(time.time())}")
         datasets = ray.get([parse_experiment_logs.remote(path) for path in paths])
         if len(datasets) > 1:
             ds_logs = (
@@ -114,7 +96,6 @@ def main():
             )
 
         output = pipeline.remote(ds_logs)
-        # output = pipeline.remote()
         ray.get(output).write_json(f"/tmp/output/{int(time.time())}")
 
 
@@ -146,35 +127,19 @@ def parse_experiment_logs(path: str) -> ray.data.Dataset:
 @ray.remote
 def pipeline(ds_logs: ray.data.Dataset) -> ray.data.Dataset:
     """
-    This pipeline.
-    Just report results (e.g. RMSE) from each run;
-    Leave statistics for notebooks.
+    This pipeline aggregates the output of
+    multiple runs from each experiment.
     """
-    # jsonl: episode, steps, returns, info (state_values)
-    # meta: {
-    # "policy_type": "single-step",
-    # "traj_mapping_method": "identity-mapper",
-    # "algorithm": "one-step-td",
-    # "reward_period": 1,
-    # "drop_truncated_episodes": false,
-    # "epsilon": 0.0,
-    # "learning_rate": 0.1,
-    # "discount_factor": 1.0,`  11`
-    # "name": "1701280078-d0c40572-StateRandomWalk-lvl1-run4"
-    # }
-    # groupby dataset by experiment_id
-    # aggregate each metric
-    # output: (ekey, episode, meta, metrics)
-
-    # result = ds_logs.aggregate(StateValueAggretator(name="state_value_agg"))
-    # return result["state_value_agg"]
-    return ds_logs.groupby("steps").count()
-
-
-def calculate_statistics(sample: Sequence[float]) -> Stat:
-    # https://www.scribbr.com/statistics/confidence-interval/
-    # assumption of normality and t-distribution
-    pass
+    return (
+        ds_logs.groupby("episode")
+        .aggregate(StateValueAggretator(name="state_value_agg"))
+        .flat_map(
+            lambda row: [
+                {"episode": row["episode"], "experiment_id": experiment_id, **data}
+                for experiment_id, data in row["state_value_agg"].items()
+            ]
+        )
+    )
 
 
 if __name__ == "__main__":
