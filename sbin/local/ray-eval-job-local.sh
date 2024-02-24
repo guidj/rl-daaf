ded0c91df238f4285e725b76f7c1ec5bb218fd88
guilherme <guilherme@spotify.com>
2024-02-24 15:04:22 +0000

    refactor: create subdir for evaljob experiments

--- sbin/local/ray-eval-job-local.sh
+++ sbin/local/ray-eval-job-local.sh
@@ -9,8 +9,8 @@ python $PARENT_DIR/src/daaf/policyeval/evaljob.py \
     --config-path=$PARENT_DIR/experiments/policyeval/experiments-sample.csv \
     --envs-path=$PARENT_DIR/experiments/policyeval/envs-sample.json \
     --num-runs=3 \
-    --num-episodes=10 \
+    --num-episodes=7500 \
     --assets-dir=$HOME/fs/daaf/assets \
-    --output-dir=$HOME/fs/daaf/exp/logs/$TIMESTAMP \
+    --output-dir=$HOME/fs/daaf/exp/evaljob/logs/$TIMESTAMP \
     --task-prefix $TIMESTAMP \
-    --log-episode-frequency=1
+    --log-episode-frequency=10
--- sbin/local/ray-eval-results-agg-job-local.sh
+++ sbin/local/ray-eval-results-agg-job-local.sh
@@ -5,8 +5,8 @@ DIR=$(dirname $0)
 PARENT_DIR=$DIR/../..
 
 DATA_DIR=$1
-INPUT_DIR=$HOME/fs/daaf/exp/logs/${DATA_DIR}
-OUTPUT_DIR=$HOME/fs/daaf/exp/agg/${DATA_DIR}/`date +%s`
+INPUT_DIR=$HOME/fs/daaf/exp/evaljob/logs/${DATA_DIR}
+OUTPUT_DIR=$HOME/fs/daaf/exp/evaljob/${DATA_DIR}/`date +%s`
 rm -rf ${OUTPUT_DIR}
 python $PARENT_DIR/src/daaf/policyeval/results_agg_pipeline.py \
     --input-dir=$INPUT_DIR \
