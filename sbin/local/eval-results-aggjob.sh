7d25d5880c48caac653a62b3841bd3bfb09d89f0
guilherme <guilherme@spotify.com>
2024-02-27 09:44:33 +0000

    refactor: output agg into subdir

--- sbin/local/eval-results-aggjob.sh
+++ sbin/local/eval-results-aggjob.sh
@@ -6,7 +6,7 @@ PARENT_DIR=$DIR/../..
 
 DATA_DIR=$1
 INPUT_DIR=$HOME/fs/daaf/exp/evaljob/logs/${DATA_DIR}
-OUTPUT_DIR=$HOME/fs/daaf/exp/evaljob/${DATA_DIR}/`date +%s`
+OUTPUT_DIR=$HOME/fs/daaf/exp/evaljob/agg/${DATA_DIR}/`date +%s`
 rm -rf ${OUTPUT_DIR}
 python $PARENT_DIR/src/daaf/policyeval/results_agg_pipeline.py \
     --input-dir=$INPUT_DIR \
