d9e1a2025a8af44ab99ab2bcf9255a73dbd50ecc
guilherme <guilherme@spotify.com>
2024-03-03 14:31:05 +0000

    refactor: local eval scripts

--- sbin/local/evalexps-evaljob.sh
+++ sbin/local/evalexps-evaljob.sh
@@ -5,11 +5,11 @@ DIR=$(dirname $0)
 PARENT_DIR=$DIR/../..
 
 TIMESTAMP=`date +%s`
-python $PARENT_DIR/src/daaf/policyeval/evaljob.py \
+python $PARENT_DIR/src/daaf/evalexps/evaljob.py \
     --config-path=$PARENT_DIR/experiments/policyeval/experiments-sample.csv \
     --envs-path=$PARENT_DIR/experiments/policyeval/envs-sample.json \
-    --num-runs=3 \
-    --num-episodes=7500 \
+    --num-runs=1 \
+    --num-episodes=10 \
     --assets-dir=$HOME/fs/daaf/assets \
     --output-dir=$HOME/fs/daaf/exp/evaljob/logs/$TIMESTAMP \
     --task-prefix $TIMESTAMP \
