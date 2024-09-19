939cd7ad9c397a540b085787a7b0ed63ba4db013
guilherme <guilherme@spotify.com>
2024-09-19 18:43:58 +0000

    refactor: fewer episodes in local run

--- sbin/local/reward-estjob.sh
+++ sbin/local/reward-estjob.sh
@@ -7,6 +7,6 @@ PARENT_DIR=$DIR/../..
 TIMESTAMP=`date +%s`
 python $PARENT_DIR/src/daaf/rewardest/estjob.py \
 --num-runs=10 \
---max-episodes=7500 \
+--max-episodes=2500 \
 --output-dir=$HOME/fs/daaf/exp/reward-estjob/logs/$TIMESTAMP \
 --log-episode-frequency=10
