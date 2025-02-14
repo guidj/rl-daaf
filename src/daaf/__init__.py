29bbfa40cdb32a0cb1aec3bcf2c5c62e56c486a3
Guilherme <guilherme@spotify.com>
2025-02-14 01:40:48 +0100

    bump version 0.15.05 -> 0.16.0

--- src/daaf/__init__.py
+++ src/daaf/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "guilherme"
-__version__ = "0.15.05"
+__version__ = "0.16.0"
 __email__ = "guilherme@dsv.su.se"
 __description__ = "RL with delayed, aggregate, feedback"
 __uri__ = "https://github.com/guidj/rl-daaf"
